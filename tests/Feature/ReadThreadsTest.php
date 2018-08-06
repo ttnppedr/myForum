ab4181cf1f9e7a39b8db7409bc4c48c1a92e7c2c
ttnppedr <priest0240@gmail.com>
2018-08-06 10:59:06 +0800

    Rename ThreadsTest to ReadThreadsTest

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ThreadsTest extends TestCase
+class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
