d31a268f88a6f9fadbfb29b78d4e0ba49c5db3f1
ttnppedr <priest0240@gmail.com>
2018-08-06 16:14:49 +0800

    Authenticated users can add replies to a threads

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -29,4 +29,15 @@ class ThreadTest extends TestCase
     {
         $this->assertInstanceOf('App\User', $this->thread->creater);
     }
+
+    /** @test */
+    public function a_thread_can_add_a_reply()
+    {
+        $this->thread->addReply([
+            'body' => 'Foobar',
+            'user_id' => 1,
+        ]);
+
+        $this->assertCount(1, $this->thread->replies);
+    }
 }
