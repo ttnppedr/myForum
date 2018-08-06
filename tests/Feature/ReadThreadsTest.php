3f77fed8bfd78db28b26daa5594b454d09602897
ttnppedr <priest0240@gmail.com>
2018-08-06 11:07:53 +0800

    Add setUp in ReadThreadsTest

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -9,21 +9,24 @@ class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
         $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
     }
 }
