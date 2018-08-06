9ea11535206fac96fb4caec9011a8e753ac36aab
ttnppedr <priest0240@gmail.com>
2018-08-06 14:27:13 +0800

    refactor threads reply blade

---
+++ resources/views/threads/reply.blade.php
@@ -0,0 +1,14 @@
+<div class="card">
+    <div class="card-header">
+        <a href="#">
+            {{ $reply->owner->name }}
+        </a> said {{ $reply->created_at->diffForHumans() }}...
+    </div>
+
+    <div class="card-body">
+
+        {{ $reply->body }}
+
+    </div>
+</div>
+<br>
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -20,20 +20,7 @@
             </div>
             <br>
             @foreach ($thread->replies as $reply)
-                <div class="card">
-                    <div class="card-header">
-                        <a href="#">
-                            {{ $reply->owner->name }}
-                        </a> said {{ $reply->created_at->diffForHumans() }}...
-                    </div>
-
-                    <div class="card-body">
-
-                        {{ $reply->body }}
-
-                    </div>
-                </div>
-            <br>
+                @include ('threads.reply')
             @endforeach
         </div>
     </div>
