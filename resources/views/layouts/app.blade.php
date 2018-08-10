9ceba9600afaf02e28c8fa6a98e67c798e14994d
ttnppedr <priest0240@gmail.com>
2018-08-10 15:19:23 +0800

    Add threads to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item"><a class="nav-link" href="/threads">All Threads</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
