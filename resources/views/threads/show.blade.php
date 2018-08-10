80699509ae05b39af45d6ce1f09cddbabe1080d8
ttnppedr <priest0240@gmail.com>
2018-08-10 15:15:25 +0800

    Update threads show view allow auth users to add a reply

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,6 +26,26 @@
             @foreach ($thread->replies as $reply)
                 @include ('threads.reply')
             @endforeach
+
+            @if(auth()->check())
+                <div class="card">
+                    <div class="card-header">
+                        Leave a Comment
+                    </div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" row="5"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-default">Post</button>
+                        </form>
+                    </div>
+                </div>
+            @else
+                <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+            @endif
         </div>
     </div>
 </div>
