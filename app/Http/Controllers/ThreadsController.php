7f33a522c82feae5cce4c5a68756135a6c68f6fa
ttnppedr <priest0240@gmail.com>
2018-08-13 15:05:05 +0800

    Change ThreadsController auth middleware

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -9,7 +9,7 @@ class ThreadsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
