f6e498d1d1f7d41d519aea817752581be150e51a
ttnppedr <priest0240@gmail.com>
2018-08-13 15:06:58 +0800

    Add threads create view

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -31,7 +31,7 @@ class ThreadsController extends Controller
      */
     public function create()
     {
-        //
+        return view('threads.create');
     }
 
     /**
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -20,6 +20,14 @@ class CreateThreadsTest extends TestCase
     }
 
     /** @test */
+    public function guest_cannot_see_the_create_page()
+    {
+        $this->withExceptionHandling()
+            ->get('/threads/create')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
         $this->signIn();
