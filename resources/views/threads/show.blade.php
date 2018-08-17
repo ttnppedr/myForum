6d48977109a809a0b47cd358ed042fc6488b51b2
ttnppedr <priest0204@gmail.com>
2018-08-17 12:56:24 +0800

    Add threads information section

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
@@ -45,6 +45,16 @@
             @else
                 <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
             @endif
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        This thread was published {{ $thread->created_at->diffForHumans() }} by <a href="#">{{ $thread->creater->name }}</a>, and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
