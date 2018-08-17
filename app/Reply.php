e719938ca1bec31bd42b397e94308f619c380d14
ttnppedr <priest0204@gmail.com>
2018-08-17 16:09:05 +0800

    Add user can favorite replies

--- app/Reply.php
+++ app/Reply.php
@@ -12,4 +12,14 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $this->favorites()->create(['user_id' => auth()->id()]);
+    }
 }
---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class FavoritesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function guests_can_not_favorite_anything()
+    {
+        $this->withExceptionHandling()
+            ->post('/replies/1/favorites')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+
+        $reply = factory('App\Reply')->create();
+
+        $this->post('/replies/' . $reply->id . '/favorites');
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
