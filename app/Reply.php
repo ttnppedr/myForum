815fddfc8bcd403cdea7c79084ad98da313367ca
ttnppedr <priest0204@gmail.com>
2018-08-23 11:27:25 +0800

    Update for eager loading

--- app/Reply.php
+++ app/Reply.php
@@ -8,6 +8,8 @@ class Reply extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['owner', 'favorites'];
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
@@ -29,6 +31,11 @@ class Reply extends Model
 
     public function isFavorited()
     {
-        return $this->favorites()->where('user_id', auth()->id())->exists();
+        return !! $this->favorites->where('user_id', auth()->id())->count();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
     }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -8,6 +8,8 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
     protected static function boot()
     {
         parent::boot();
@@ -24,9 +26,7 @@ class Thread extends Model
 
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     public function creator()
