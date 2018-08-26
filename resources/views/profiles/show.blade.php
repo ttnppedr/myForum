132421fa35799d545c090bc10a84a3d58b8a5d0a
ttnppedr <priest0204@gmail.com>
2018-08-26 12:15:10 +0800

    Rearrange profiles show view

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,41 +2,40 @@
 
 @section('content')
     <div class="container">
-        <div class="border-bottom">
-            <h1>
-                {{ $profileUser->name }}
-                <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-            </h1>
-        </div>
-
-        @foreach ($threads as $thread)
-            <br>
-            <div class="card">
-                <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="#">{{ $thread->creator->name }}</a> posted
-                            {{ $thread->title }}
-                        </span>
-
-                        <span>{{ $thread->created_at->diffForHumans() }}</span>
-                    </div>
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="border-bottom">
+                    <h1>
+                        {{ $profileUser->name }}
+                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+                    </h1>
                 </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                @foreach ($threads as $thread)
+                    <br>
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="level">
+                                <span class="flex">
+                                    <a href="#">{{ $thread->creator->name }}</a> posted
+                                    {{ $thread->title }}
+                                </span>
+
+                                <span>{{ $thread->created_at->diffForHumans() }}</span>
+                            </div>
                         </div>
-                    @endif
 
-                    {{ $thread->body }}
+                        <div class="card-body">
 
-                </div>
+                            {{ $thread->body }}
+
+                        </div>
+                    </div>
+                @endforeach
+
+                <br>
+                {{ $threads->links() }}
             </div>
-        @endforeach
-
-        <br>
-        {{ $threads->links() }}
+        </div>
     </div>
 @endsection
