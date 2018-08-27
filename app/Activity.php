77c922ef5c07a01a34487c83d3451729f4f8d566
ttnppedr <priest0204@gmail.com>
2018-08-27 14:14:57 +0800

    refactor activity feed

--- app/Activity.php
+++ app/Activity.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
@@ -11,5 +12,17 @@ class Activity extends Model
     public function subject()
     {
         return $this->morphTo();
+    }
+
+    public static function feed(User $user, $take = 50)
+    {
+        return static::where('user_id', $user->id)
+            ->latest()
+            ->with('subject')
+            ->take($take)
+            ->get()
+            ->groupBy(function ($activity) {
+                return $activity->created_at->format('Y-m-d');
+        });
     }
 }
