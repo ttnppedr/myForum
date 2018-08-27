9f23f4fb8e8f3e6043004a6c43ddc917b3136b06
ttnppedr <priest0204@gmail.com>
2018-08-27 15:03:49 +0800

    Fix activity reference bug when delete thread

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Activity;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -88,6 +89,17 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+
+        $this->assertDatabaseMissing('activities', [
+            'subject_id' => $thread->id,
+            'subject_type' => get_class($thread),
+        ]);
+        $this->assertDatabaseMissing('activities', [
+            'subject_id' => $reply->id,
+            'subject_type' => get_class($reply),
+        ]);
+
+        $this->assertEquals(0, Activity::count());
     }
 
     protected function publishThread($overrides = [])
