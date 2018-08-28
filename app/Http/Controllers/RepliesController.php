527e7558882409b1deabe8189fde43a6ef31a1ca
ttnppedr <priest0204@gmail.com>
2018-08-28 16:06:52 +0800

    Add users can delete replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Reply;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -24,5 +25,14 @@ class RepliesController extends Controller
         ]);
 
         return back()->with('flash', 'Your reply has been left.');
+    }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+
+        $reply->delete();
+
+        return back();
     }
 }
