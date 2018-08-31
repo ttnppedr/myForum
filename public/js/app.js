d0cfb88ee61be2fdf23d8712a984f2a1d71a0209
ttnppedr <priest0204@gmail.com>
2018-08-31 15:56:49 +0800

    Fix delete wrong replies

--- public/js/app.js
+++ public/js/app.js
@@ -48581,6 +48581,7 @@ var render = function() {
       _vm._l(_vm.items, function(reply, index) {
         return _c(
           "div",
+          { key: reply.id },
           [
             _c("reply", {
               attrs: { data: reply },
