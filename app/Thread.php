d2e7d122289c67574e2dbe8a3d1e6eb7ad452d5c
ttnppedr <priest0204@gmail.com>
2018-09-07 11:31:01 +0800

    Refactor Thread addReply

--- app/Thread.php
+++ app/Thread.php
@@ -47,13 +47,17 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->subscriptions
-            ->filter(function ($sub) use ($reply) {
-                return $sub->user_id != $reply->user_id;
-            })
-            ->each->notify($reply);
+        $this->notifySubscribers($reply);
 
         return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
+        $this->subscriptions
+            ->where('user_id', '!=', $reply->user_id)
+            ->each
+            ->notify($reply);
     }
 
     public function scopeFilter($query, $filters)
