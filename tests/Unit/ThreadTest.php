e8f5e2fad88ad36e9260192de33c6b758f64f948
ttnppedr <priest0204@gmail.com>
2018-09-07 15:00:40 +0800

    Add a thread notifies subscribers test

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
+use App\Notifications\ThreadWasUpdated;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
 
 class ThreadTest extends TestCase
 {
@@ -48,6 +50,22 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1, $this->thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+
+        $this->signIn()
+            ->thread
+            ->subscribe()
+            ->addReply([
+                'body' => 'Foobar',
+                'user_id' => 999,
+            ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
     }
 
     /** @test */
