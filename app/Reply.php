9ef731f669008eb1ef82099da34433c77bb3a249
ttnppedr <priest0204@gmail.com>
2018-09-18 11:10:16 +0800

    Add wrap usernames with anchor tag

--- app/Reply.php
+++ app/Reply.php
@@ -50,7 +50,7 @@ class Reply extends Model
 
     public function mentionedUsers()
     {
-        preg_match_all('/\@([^\s\.]+)/', $this->body, $matches);
+        preg_match_all('/@([\w\-]+)/', $this->body, $matches);
 
         return $matches[1];
     }
@@ -58,5 +58,10 @@ class Reply extends Model
     public function path()
     {
         return $this->thread->path() . "#reply-{$this->id}";
+    }
+
+    public function setBodyAttribute($body)
+    {
+        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body);
     }
 }
