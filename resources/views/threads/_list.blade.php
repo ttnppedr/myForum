076c5d0d65c3382b268d2f2caf919b50dc431e9e
ttnppedr <priest0204@gmail.com>
2018-09-21 15:52:09 +0800

    Refactor threads index page

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,38 @@
+@forelse ($threads as $thread)
+    <div class="card">
+        <div class="card-header">
+            <div class="level">
+                <div class="flex">
+                    <h4>
+                        <a href="{{ $thread->path() }}">
+                            @if (auth()->check() && $thread->hasUpdatedFor(auth()->user()))
+                                <strong>
+                                    {{ $thread->title }}
+                                </strong>
+                            @else
+                                {{ $thread->title }}
+                            @endif
+                        </a>
+                    </h4>
+
+                    <h5>
+                        Posted By: <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
+                    </h5>
+                </div>
+
+                <a href="{{ $thread->path() }}">
+                    {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count)}}
+                </a>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <div class="body">
+                {{ $thread->body }}
+            </div>
+        </div>
+    </div>
+    <br>
+@empty
+    <p>There are no relevant results at this time.</p>
+@endforelse
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,38 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @forelse ($threads as $thread)
-                <div class="card">
-                    <div class="card-header">
-                        <div class="level">
-                            <h4 class="flex">
-                                <a href="{{ $thread->path() }}">
-                                    @if (auth()->check() && $thread->hasUpdatedFor(auth()->user()))
-                                        <strong>
-                                            {{ $thread->title }}
-                                        </strong>
-                                    @else
-                                        {{ $thread->title }}
-                                    @endif
-                                </a>
-                            </h4>
+            @include('threads._list')
 
-                            <a href="{{ $thread->path() }}">
-                                {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count)}}
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        <div class="body">
-                            {{ $thread->body }}
-                        </div>
-                    </div>
-                </div>
-                <br>
-            @empty
-                <p>There are no relevant results at this time.</p>
-            @endforelse
+            {{ $threads->render() }}
         </div>
     </div>
 </div>
