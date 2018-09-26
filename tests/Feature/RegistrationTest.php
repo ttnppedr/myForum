7f3bcb73e93f979ef13c28c4c863d430f9f1b04f
ttnppedr <priest0204@gmail.com>
2018-09-26 08:52:08 +0800

    Refactor improve confirmation_token design

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -47,7 +47,10 @@ class RegistrationTest extends TestCase
         $response = $this->get(route('register.confirm', ['token' => $user->confirmation_token]))
              ->assertRedirect(route('threads'));
 
-        $this->assertTrue($user->refresh()->confirmed);
+        tap($user->fresh(), function ($user) {
+            $this->assertTrue($user->confirmed);
+            $this->assertNull($user->confirmation_token);
+        });
     }
 
     /** @test **/
