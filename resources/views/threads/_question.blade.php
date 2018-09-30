64ce6ae56a542cc4e112dc6f25a2102a9b88b6ba
ttnppedr <priest0204@gmail.com>
2018-09-30 10:22:59 +0800

    Add editing thread ajax in vue

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -2,21 +2,21 @@
 <div class="card" v-if="editing">
     <div class="card-header">
         <div class="level">
-            <input type="text" value="{{ $thread->title }}" class="form-control">
+            <input type="text" class="form-control" v-model="form.title">
         </div>
     </div>
 
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10">{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
         </div>
     </div>
 
     <div class="card-footer">
         <div class="level">
             <button class="btn btn-sm mr-1" @click="editing = true" v-show="! editing">Edit</button>
-            <button class="btn btn-sm btn-primary mr-1" @click="">Update</button>
-            <button class="btn btn-sm btn-link mr-1" @click="editing = false">Cancel</button>
+            <button class="btn btn-sm btn-primary mr-1" @click="update">Update</button>
+            <button class="btn btn-sm btn-link mr-1" @click="resetForm">Cancel</button>
 
             @can ('update', $thread)
                 <form action="{{ $thread->path() }}" method="POST" class="ml-a">
@@ -37,16 +37,14 @@
             <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-1">
             <span class="flex">
                 <a href="{{ route('profile', $thread->creator->name) }}">{{ $thread->creator->name }}</a> posted
-                {{ $thread->title }}
+                <span v-text="title"></span>
             </span>
         </div>
     </div>
 
-    <div class="card-body">
-        {{ $thread->body }}
-    </div>
+    <div class="card-body" v-text="body"></div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-sm" @click="editing = true">Edit</button>
     </div>
 </div>
