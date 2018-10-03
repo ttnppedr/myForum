7dc925d85f9daaba6aff152a9c4c32d60338e6d8
ttnppedr <priest0204@gmail.com>
2018-10-03 10:42:16 +0800

    Add sanitize before display user input

--- app/Reply.php
+++ app/Reply.php
@@ -74,4 +74,9 @@ class Reply extends Model
     {
         return $this->isBest();
     }
+
+    public function getBodyAttribute($body)
+    {
+        return \Purify::clean($body);
+    }
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -132,4 +132,12 @@ class ThreadTest extends TestCase
             $this->assertFalse($thread->hasUpdatedFor($user));
         });
     }
+
+    /** @test **/
+    public function a_thread_body_is_sanitized_automatically()
+    {
+        $thread = make('App\Thread', ['body' => '<script>alert("bad")</script><p>This is okay.</p>']);
+
+        $this->assertEquals('<p>This is okay.</p>', $thread->body);
+    }
 }
