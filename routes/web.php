18ccbec31c48c02bc963ab9c591c162f1b361e1d
ttnppedr <priest0204@gmail.com>
2018-10-05 10:33:38 +0800

    Change index route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ThreadsController@index');
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
