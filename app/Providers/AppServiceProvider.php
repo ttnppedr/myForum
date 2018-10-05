70dc8361cbb599d1a8724b81bdd3958585ccaa9d
ttnppedr <priest0204@gmail.com>
2018-10-05 12:49:28 +0800

    Fix insecure script

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \URL::forceScheme('https');
         Schema::defaultStringLength(191);
         View::composer('*', function ($view) {
             $channels = Cache::rememberForever('channels', function () {
